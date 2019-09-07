5e2c33c28990432539529743bd7fd50f7aaf378e
Dillon Lue <dyl3pc@virginia.edu>
2019-09-06 20:36:07 -0400

    Added a basic checker

---
+++ checker.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+file_to_check="knapsack"
+cd /Users/dillonlue/Desktop/ICPC
+cat ${file_to_check}/1.in | python3 ${file_to_check}/code.py
+# cat ${file_to_check}/1.in | python3 ${file_to_check}/code.py > answers.txt
